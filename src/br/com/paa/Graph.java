b47b2c3073d62eaec72a48f28d6be21c56f0f249
Lidiomar Fernando dos Santos Machado <fernandoMachado@Mac-mini-de-Lidiomar.local>
2018-05-08 00:41:37 -0300

    Eulerian Cycle

--- src/br/com/paa/Graph.java
+++ src/br/com/paa/Graph.java
@@ -1,21 +1,29 @@
 package br.com.paa;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class Graph {
 	
 	private LinkedList<Integer> vertexList[];
 	private boolean visited[];
+	private Stack<Integer> stack;
+	private ArrayList<Integer> circuit;
 	
 	public Graph(int size) {
 		vertexList = new LinkedList[size];
 		visited = new boolean[size];
+		stack = new Stack<Integer>();
+		circuit = new ArrayList<Integer>();
 		
 		for(int i = 0; i < size; i ++) {
 			vertexList[i] = new LinkedList<Integer>();
 		}
 	}
+	
+	
 	
 	public void dfs(int vertex) {
 		visited[vertex] = true;
@@ -39,7 +47,7 @@ public class Graph {
 	/* 0: Is not Eulerian
 	 * 1: Eulerian Cycle 
 	 * 2: Eulerian Path*/
-	public int getGraphType() {
+	public void traversal() {
 		int oddEdges = 0;
 		
 		for(int i=0; i < vertexList.length; i++) {
@@ -48,7 +56,6 @@ public class Graph {
 			
 			if(edges == 0) {
 				System.out.println("Is not Eulerian");
-				return 0;
 			}
 			
 			if(edges % 2 != 0) {
@@ -57,15 +64,44 @@ public class Graph {
 		}
 		
 		if(oddEdges == 0) {
-			System.out.println("Eulerian Cycle");
-			return 1;
+			System.out.print("Eulerian Cycle:\n\n");
+			eulerianCycle(0);
+			
+			
+			System.out.print(circuit.get(circuit.size() -1) + "");
+			for(int i = circuit.size() -2; i >= 0; i--) {
+				System.out.print(" -> "+circuit.get(i));
+			}
+			
 		}else if(oddEdges == 2) {
 			System.out.println("Eulerian Path");
-			return 2;
+			
 		}else {
 			System.out.println("Is not Eulerian");
-			return 0;
 		}
 	}
-
+	
+	public void eulerianCycle(Integer currentVertex) {
+		
+		LinkedList<Integer> edges = vertexList[currentVertex];
+		Iterator<Integer> iterator = edges.listIterator();
+		
+		if(iterator.hasNext()) {
+			stack.push(currentVertex);
+			Integer nextVertex = iterator.next();
+			edges.remove(nextVertex);
+			vertexList[nextVertex].remove(currentVertex);
+			eulerianCycle(nextVertex);
+		}else if(!stack.isEmpty()){
+			Integer vertexFromStack = stack.pop();
+			circuit.add(currentVertex);
+			
+			if(!stack.isEmpty()) {
+				eulerianCycle(vertexFromStack);
+			}else {
+				circuit.add(vertexFromStack);
+			}
+		}
+		
+	}
 }
--- src/br/com/paa/Main.java
+++ src/br/com/paa/Main.java
@@ -2,33 +2,43 @@ package br.com.paa;
 
 public class Main {
 	public static void main(String ... args) {
-		Graph g1 = new Graph(4);
-        g1.addEdge(0, 1);
-        g1.addEdge(0, 2);
-        g1.addEdge(1, 0);
-        g1.addEdge(1, 2);
-        g1.addEdge(2, 0);
-        g1.addEdge(2, 1);
-        g1.addEdge(2, 3);
-        g1.addEdge(3, 2);
-        
-        g1.getGraphType();
- 
-        /*Graph g2 = new Graph(3);
-        g2.addEdge(0, 1);
-        g2.addEdge(1, 2);
-        g2.addEdge(2, 0);
-        g2.getGraphType();
-        
-        Graph g3 = new Graph(5);
-        g3.addEdge(1, 0);
-        g3.addEdge(0, 2);
-        g3.addEdge(2, 1);
-        g3.addEdge(0, 3);
-        g3.addEdge(3, 4);
-        g3.addEdge(3, 2);
-        g3.addEdge(3, 1);
-        g3.addEdge(2, 4);
-        g3.getGraphType();*/
+		
+		Graph g1 = new Graph(9);
+		
+	    g1.addEdge(0, 1);
+	    g1.addEdge(0, 5);
+	    g1.addEdge(0, 7);
+	    g1.addEdge(0, 8);
+	    
+	    g1.addEdge(1, 0);
+	    g1.addEdge(1, 2);
+	    g1.addEdge(1, 3);
+	    g1.addEdge(1, 7);
+	    
+	    g1.addEdge(2, 1);
+	    g1.addEdge(2, 3);
+	    
+	    g1.addEdge(3, 1);
+	    g1.addEdge(3, 2);
+	    
+	    g1.addEdge(4, 6);
+	    g1.addEdge(4, 7);
+	    
+	    g1.addEdge(5, 0);
+	    g1.addEdge(5, 8);
+	    
+	    g1.addEdge(6, 4);
+	    g1.addEdge(6, 7);
+	    
+	    g1.addEdge(7, 0);
+	    g1.addEdge(7, 1);
+	    g1.addEdge(7, 4);
+	    g1.addEdge(7, 6);
+	    
+	    g1.addEdge(8, 0);
+	    g1.addEdge(8, 5);
+	    
+	    g1.traversal();
+       
 	}
 }
