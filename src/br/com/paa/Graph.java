e288508e470172ac576b0b6b57f7374adf229f94
Lidiomar Fernando dos Santos Machado <fernandoMachado@Mac-mini-de-Lidiomar.local>
2018-05-24 01:09:33 -0300

    tests

--- src/br/com/paa/Graph.java
+++ src/br/com/paa/Graph.java
@@ -1,6 +1,7 @@
 package br.com.paa;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Stack;
@@ -8,17 +9,15 @@ import java.util.Stack;
 public class Graph {
 	
 	private LinkedList<Integer> vertexList[];
-	private boolean visited[];
 	private Stack<Integer> stack;
 	private ArrayList<Integer> circuit;
 	
 	public Graph(int size) {
-		vertexList = new LinkedList[size]; // O(1)
-		visited = new boolean[size]; 
+		vertexList = new LinkedList[size]; // O(1) 
 		stack = new Stack<Integer>();
 		circuit = new ArrayList<Integer>();
 		
-		for(int i = 0; i < size; i ++) { // O(n) ??
+		for(int i = 0; i < size; i ++) { // O(n)
 			vertexList[i] = new LinkedList<Integer>(); // O(1)
 		}
 	}
@@ -27,13 +26,12 @@ public class Graph {
 		vertexList[vertex].add(edge); // O(1)
 	}
 	
-	
+	/*Complexidade O(n), 
+	onde n √© o n√∫mero de v√©rtices */
 	public void traversal() {
 		
-		for(int i=0; i < vertexList.length; i++) {
-			
-			int edges = vertexList[i].size();
-			
+		for(int i=0; i < vertexList.length; i++) { // O(n)		
+			int edges = vertexList[i].size();		
 			if(edges == 0) {
 				System.out.println("Is not Eulerian");
 				return;
@@ -45,8 +43,7 @@ public class Graph {
 			}
 		}
 		
-		
-		System.out.print("Eulerian Cycle:\n\n");
+		System.out.println("Eulerian Cycle:\n\n");
 		eulerianCycle(0);
 		
 		System.out.print(circuit.get(circuit.size() -1) + "");
@@ -55,9 +52,8 @@ public class Graph {
 			System.out.print(" -> "+circuit.get(i));
 		}
 	}
-	/*Complexidade O(n + v), 
-	onde n È o n˙mero de elementos e v È o n˙mero de vÈrtices, sendo a complexidade de
-	O(n) */
+	
+
 	public void eulerianCycle(Integer currentVertex) {
 		
 		LinkedList<Integer> edges = vertexList[currentVertex];
@@ -83,17 +79,86 @@ public class Graph {
 		
 	}
 	
-	public void dfs(int vertex) {
-		visited[vertex] = true;
-		System.out.println("vertex: -> " + vertex);
-		
-		Iterator<Integer> iterator = vertexList[vertex].listIterator();
-		
-		while (iterator.hasNext()) {
-			int nextVertex = iterator.next();
-			if(!visited[nextVertex]) {
-				dfs(nextVertex);
+	/*Complexidade O(n), 
+	onde n √© o n√∫mero de v√©rtices */
+	public void traversal2() {
+		for(int i=0; i < vertexList.length; i++) { // O(n)
+			int edges = vertexList[i].size();
+			
+			if(edges == 0) {
+				System.out.println("Is not Eulerian");
+				return;
 			}
+			
+			if(edges % 2 != 0) {
+				System.out.println("Is not Eulerian Cycle");
+				return;
+			}
+		}
+		
+		System.out.println("\n\nEulerian Cycle:\n\n");
+		eulerianCycle2(0);
+		
+		System.out.print(circuit.get(0) + "");
+		
+		for(int i = 1; i < circuit.size(); i++) {
+			System.out.print(" -> "+circuit.get(i));
+		}
+	}
+	
+	public void eulerianCycle2(Integer currentVertex) {
+		LinkedList<Integer> edges = vertexList[currentVertex];
+		Iterator<Integer> iterator = edges.listIterator();
+		circuit.add(currentVertex);
+		
+		if(iterator.hasNext()) {	
+			Integer nextVertex = iterator.next();
+			edges.remove(nextVertex);
+			vertexList[nextVertex].remove(currentVertex);
+			
+			eulerianCycle2(nextVertex);
+		}
+	}
+	
+	/*Complexidade O(n * nLogN), 
+	onde n √© o n√∫mero de v√©rtices */
+	public void traversal3() {
+		for(int i=0; i < vertexList.length; i++) { // O(n)
+			int edges = vertexList[i].size();
+			
+			if(edges == 0) {
+				System.out.println("Is not Eulerian");
+				return;
+			}
+			
+			if(edges % 2 != 0) {
+				System.out.println("Is not Eulerian Cycle");
+				return;
+			}
+		}
+		
+		System.out.println("\n\nEulerian Cycle:\n\n");
+		eulerianCycle3(0);
+		
+		System.out.print(circuit.get(0) + "");
+		
+		for(int i = 1; i < circuit.size(); i++) {
+			System.out.print(" -> "+circuit.get(i));
+		}
+	}
+	
+	public void eulerianCycle3(Integer currentVertex) {
+		LinkedList<Integer> edges = vertexList[currentVertex];
+		Collections.sort(edges, Collections.reverseOrder()); //O(nLogN)
+		Iterator<Integer> iterator = edges.listIterator();
+		
+		if(iterator.hasNext()) {
+			circuit.add(currentVertex);
+			Integer nextVertex = iterator.next();
+			edges.remove(nextVertex);
+			vertexList[nextVertex].remove(currentVertex);
+			
+			eulerianCycle3(nextVertex);
 		}
 	}
 	
--- src/br/com/paa/Main.java
+++ src/br/com/paa/Main.java
@@ -37,7 +37,82 @@ public class Main {
 	    g1.addEdge(8, 0);
 	    g1.addEdge(8, 5);
 	    
-	    g1.traversal();
+	    //g1.traversal();
+	    
+	    Graph g2 = new Graph(9);
+		
+	    g2.addEdge(0, 8);
+	    g2.addEdge(0, 7);
+	    g2.addEdge(0, 5);
+	    g2.addEdge(0, 1) ;
+	    
+	    g2.addEdge(1, 7);
+	    g2.addEdge(1, 3);
+	    g2.addEdge(1, 2);
+	    g2.addEdge(1, 0);
+	    
+	    g2.addEdge(2, 3);
+	    g2.addEdge(2, 1);
+	    
+	    g2.addEdge(3, 2);
+	    g2.addEdge(3, 1);
+	    
+	    g2.addEdge(4, 7);
+	    g2.addEdge(4, 6);
+	    
+	    g2.addEdge(5, 8);
+	    g2.addEdge(5, 0);
+	    
+	    g2.addEdge(6, 7);
+	    g2.addEdge(6, 4);
+	    
+	    g2.addEdge(7, 6);
+	    g2.addEdge(7, 4);
+	    g2.addEdge(7, 1);
+	    g2.addEdge(7, 0);
+	    
+	    g2.addEdge(8, 5);
+	    g2.addEdge(8, 0);
+	    
+	    g2.traversal2();
+	    
+	    
+	    Graph g3 = new Graph(9);
+		
+	    g3.addEdge(0, 1);
+	    g3.addEdge(0, 7);
+	    g3.addEdge(0, 5);
+	    g3.addEdge(0, 8) ;
+	    
+	    g3.addEdge(1, 0);
+	    g3.addEdge(1, 3);
+	    g3.addEdge(1, 2);
+	    g3.addEdge(1, 7);
+	    
+	    g3.addEdge(2, 1);
+	    g3.addEdge(2, 3);
+	    
+	    g3.addEdge(3, 1);
+	    g3.addEdge(3, 2);
+	    
+	    g3.addEdge(4, 6);
+	    g3.addEdge(4, 7);
+	    
+	    g3.addEdge(5, 0);
+	    g3.addEdge(5, 8);
+	    
+	    g3.addEdge(6, 4);
+	    g3.addEdge(6, 7);
+	    
+	    g3.addEdge(7, 0);
+	    g3.addEdge(7, 4);
+	    g3.addEdge(7, 1);
+	    g3.addEdge(7, 6);
+	    
+	    g3.addEdge(8, 0);
+	    g3.addEdge(8, 5);
+	    
+	    //g3.traversal3();
        
 	}
 }
